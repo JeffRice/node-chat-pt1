8b48394c569ba044d11365ee865fb2c54faab319
Ubuntu <ubuntu@ip-10-145-242-230.ec2.internal>
2014-07-23 03:29:54 +0000

    added whisper capability

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
-    nicknames = [];
+    users = {};
 
 server.listen(8080);
 
@@ -14,29 +14,50 @@ app.get('/', function(req, res){
 io.sockets.on('connection', function(socket) {
 
 	socket.on('new user', function(data, callback) {
-		if (nicknames.indexOf(data) != -1){
+		if (data in users){
 			callback(false);
 		} else {
 			callback(true);
 			socket.nickname = data;
-			nicknames.push(socket.nickname);
+			users[socket.nickname] = socket;
 	  	    updateNicknames();
 		}
 	});
 
 
 	function updateNicknames() {
-		io.sockets.emit('usernames', nicknames)
+		io.sockets.emit('usernames', Object.keys(users));
 	}
 
 
-	socket.on('send message', function(data) {
-		io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+	socket.on('send message', function(data, callback) {
+		var msg = data.trim();
+		if (msg.substr(0,3) === '/w ') {
+			msg = msg.substr(3);
+			var ind = msg.indexOf(' ');
+			if (ind!== -1){
+				var name = msg.substring(0, ind);
+				var msg = msg.substring(ind + 1);
+				if (name in users) {
+					users[name].emit('whisper', {msg: msg, nick: socket.nickname});
+				console.log('Whisper')
+				}
+				else{
+				callback('you can only message a current user')
+				}
+			}
+			else{
+				callback('Error! please enter a message')
+			}
+		}
+		else {
+		io.sockets.emit('new message', {msg: msg, nick: socket.nickname});
+		}
 	});
 
 	socket.on('disconnect', function(data) {
 		if(!socket.nickname) return;
-		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		delete users[socket.nickname];
 	    updateNicknames();
 	});
 
